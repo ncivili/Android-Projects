60bd2d448857d0c91e307265621a017b17205225
Nick <nick@Nicks-MacBook-Pro.local>
2015-04-20 20:10:50 -0500

    Code for about screen activity
    Added about screen activity and menu bar options

--- CannonGame/app/src/main/java/com/npcf34/android/cannongame/AboutActivity.java
+++ CannonGame/app/src/main/java/com/npcf34/android/cannongame/AboutActivity.java
@@ -1,25 +1,24 @@
 package com.npcf34.android.cannongame;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AboutActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_about, menu);
         return true;
     }
 
@@ -30,15 +29,9 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch(id) {
-            case R.id.action_about:
-                //about screen
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.action_reset:
-                //reset game stats
-                break;
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
